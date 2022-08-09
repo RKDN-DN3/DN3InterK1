885fffc22bad827e63fce064b0f92cab8dd91ee4
Huy <huyln_tts@rikkeisoft.com>
2022-08-09 14:49:58 +0700

    Add Model + Seed Data Answer

---
+++ Quiz/Quiz.Entities/Answer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Entities
+{
+    public class Answer : BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [Column(Order = 0)]
+        public int SEQ { get; set; }
+
+
+        
+        [Key]
+        [Column(Order = 1)]
+        [Display(Name = "ID Of Question ")]
+        [StringLength(10)]
+        public string ID_Question { get; set; }
+
+
+        [Required]
+        [StringLength(1)]
+        [Display(Name = "Flag IsCorrect")]
+        public string IsCorrect { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Content Of Question")]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(1)]
+        [Display(Name = "Flag IsDelete")]
+        public string IsDelete { get; set; }
+
+
+    }
+}
