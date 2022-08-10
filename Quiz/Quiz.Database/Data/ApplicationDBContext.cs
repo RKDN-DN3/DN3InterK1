52947f40823518993bb13333ddb4dc45f2d7c2ec
Huy <huyln_tts@rikkeisoft.com>
2022-08-10 14:49:43 +0700

    Done QUIZ-84

--- Quiz/Quiz.Database/Data/ApplicationDBContext.cs
+++ Quiz/Quiz.Database/Data/ApplicationDBContext.cs
@@ -17,16 +17,19 @@ namespace Quiz.Database.Data
         public DbSet<Question_Bank> Question_Banks { get; set; }
         public DbSet<Examination> Examinations { get; set; }
         public DbSet<Examination_Detail> Examination_Details{ get; set; }
-
+        public DbSet<Answer> Answers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Examination_Detail>().HasKey(c => new { c.Id_Exam, c.Id_Question_Bank});
             modelBuilder.Entity<Examination>()
                 .HasMany(p => p.Examination_Details)
                 .WithOne(p => p.Examination)
                 .HasForeignKey(c => c.Id_Exam);
+
+            modelBuilder.Entity<Answer>().HasKey(c => new { c.SEQ, c.ID_Question });
         }
     }
 }
---
+++ Quiz/Quiz.Entities/Answer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Entities
+{
+    public class Answer : BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(Order = 0)]
+        public int SEQ { get; set; }
+
+
+        [Column(Order = 1)]
+        [Display(Name = "ID Of Question ")]
+        [StringLength(10)]
+        public string ID_Question { get; set; }
+
+
+        [Required]
+        [StringLength(1)]
+        [Display(Name = "Flag IsCorrect")]
+        public string IsCorrect { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Content Of Question")]
+        public string Content { get; set; }
+
+        [Required]
+        [StringLength(1)]
+        [Display(Name = "Flag IsDelete")]
+        public string IsDelete { get; set; }
+
+    }
+}
