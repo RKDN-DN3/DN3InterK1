04bb208affdc9eca299191efd28e809fbce1feee
Huy <huyln_tts@rikkeisoft.com>
2022-08-12 17:57:59 +0700

    seed data exam_history and exam_history_Detail

--- Quiz/Quiz.Database/Migrations/SeedData_Exam_History.cs
+++ Quiz/Quiz.Database/Migrations/SeedData_Exam_History.cs
@@ -9,20 +9,55 @@ using System.Threading.Tasks;
 
 namespace Quiz.Database.Migrations
 {
-    //public class SeedData_Exam_History
-    //{
-    //    public static void Initialize(IServiceProvider serviceProvider)
-    //    {
-    //        using (var context = new ApplicationDBContext(
-    //            serviceProvider.GetRequiredService<
-    //                DbContextOptions<ApplicationDBContext>>()))
-    //        {
+    public class SeedData_Exam_History
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDBContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDBContext>>()))
+            {
+                
+                if (context.Exam_Historys.Any())
+                    {
+                        return;   
+                    }
+                context.Exam_Historys.AddRange(
+                    new Entities.Exam_History
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        DateDoExam = DateTime.Now,
+                        Mark = 10,
+                        TimeDoExam = 30,
+                        StartTime = DateTime.Now,
+                        EndTime = DateTime.Now,
+                        IsDelete = "0"
+                    },
+                    new Entities.Exam_History
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        DateDoExam = DateTime.Now,
+                        Mark = 9,
+                        TimeDoExam = 30,
+                        StartTime = DateTime.Now,
+                        EndTime = DateTime.Now,
+                        IsDelete = "0"
+                    },
+                    new Entities.Exam_History
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        DateDoExam = DateTime.Now,
+                        Mark = 8,
+                        TimeDoExam = 30,
+                        StartTime = DateTime.Now,
+                        EndTime = DateTime.Now,
+                        IsDelete = "0"
+                    }
 
-    //            if (context..Any())
-    //            {
-    //                return;
-    //            }
-    //        }
-    //    }
-    //}
+                    );
+
+            }
+        }
+
+    }
 }
---
+++ Quiz/Quiz.Database/Migrations/SeedData_Exam_History_Detail.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Quiz.Database.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Database.Migrations
+{
+    public class SeedData_Exam_History_Detail
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDBContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDBContext>>()))
+            {
+
+                if (context.Exam_History_Details.Any())
+                {
+                    return;
+                }
+                context.Exam_History_Details.AddRange(
+                    new Entities.Exam_History_Detail
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        Email_User = "thanhnq@gmail.com",
+                        Date_Do_Exam = DateTime.Now,
+                        ID_Question = new Guid("86bb6ed2-e395-4cb0-a0ef-0423b19c09bf"),
+                        ID_Answer_Chose = "123",
+                        IsDelete = "0"
+                    },
+                    new Entities.Exam_History_Detail
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        Email_User = "trieu@gmail.com",
+                        Date_Do_Exam = DateTime.Now,
+                        ID_Question = new Guid("f8bcc8ea-001c-4011-a213-fb9e1e20785a"),
+                        ID_Answer_Chose = "456",
+                        IsDelete = "0"
+                    },
+                    new Entities.Exam_History_Detail
+                    {
+                        ID_Exam = Guid.NewGuid(),
+                        Email_User = "huyln@gmail.com",
+                        Date_Do_Exam = DateTime.Now,
+                        ID_Question = new Guid("d5d25730-9353-4a57-952e-0c39d2614b7c"),
+                        ID_Answer_Chose = "789",
+                        IsDelete = "0"
+                    });
+                    
+            }
+        }
+    }
+}
