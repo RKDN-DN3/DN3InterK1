5cb4a10375886c5c1f36a89c1cbbc208c79c0c01
Thanh <thanhnq_tts@rikkeisoft.com>
2022-08-17 10:34:54 +0700

    Done Quiz-24 : Add new question

--- Quiz/Quiz.Database/ViewModels/QuestionsVM.cs
+++ Quiz/Quiz.Database/ViewModels/QuestionsVM.cs
@@ -1,10 +1,12 @@
-﻿using Quiz.Entities;
+﻿using Microsoft.AspNetCore.Http;
+using Quiz.Entities;
 
 namespace Quiz.Database.ViewModels
 {
     public class QuestionsVM
     {
         public Question question { get; set; } = new Question();
+        public IFormFile File { get; set; }
 
         public IEnumerable<Question> questions { get; set; } = new List<Question>();
 
