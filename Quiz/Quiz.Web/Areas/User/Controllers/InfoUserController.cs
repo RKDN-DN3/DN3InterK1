15cb5a0ff79d50d877370171964de5a96b368c34
Huy <huyln_tts@rikkeisoft.com>
2022-09-09 11:54:23 +0700

    commit

---
+++ Quiz/Quiz.Web/Areas/User/Controllers/InfoUserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.Database.Repositories;
+using Quiz.Database.ViewModels;
+using Quiz.Web.Controllers;
+
+namespace Quiz.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class InfoUserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IUnitOfWork _unitoWork;
+
+        public InfoUserController(IUnitOfWork unitoWork, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _unitoWork = unitoWork;
+        }
+        public IActionResult Index()
+        {
+            AccountsVM vM = new AccountsVM();
+            vM.account = _unitoWork.Account.GetAll().Where(p => p.Email_User == "huyln@gmail.com").FirstOrDefault();
+            if (vM.account == null)
+            {
+                return NotFound();
+            }
+            return View(vM);
+        }
+    }
+
+}
