26b9c2a3c9862545c7eda49609d0d36ee670637d
anhttran <anhttran@kms-technology.com>
2022-09-16 05:36:11 +0700

    validation, dropdown, update layout, fixbug

--- Quiz/Quiz.Entities/Question.cs
+++ Quiz/Quiz.Entities/Question.cs
@@ -4,15 +4,14 @@ namespace Quiz.Entities
 {
     public class Question : BaseEntity
     {
-        public Question()
-        {
-            this.Answers = new HashSet<Answer>();
-        }
+        public Question() => this.Answers = new HashSet<Answer>();
 
         [Key]
         public Guid Id { get; set; }
 
         public Guid Id_Question_Bank { get; set; }
+
+        [Required]
         public string? Content { get; set; }
         public string? ImageUrl { get; set; }
 
@@ -23,6 +22,7 @@ namespace Quiz.Entities
         [StringLength(100)]
         public string? Type { get; set; }
 
+        [Required]
         public string? Explaint { get; set; }
         public virtual ICollection<List_Question_In_Exam>? List_Question_In_Exams { get; set; }
         public virtual Question_Bank? Question_Bank { get; set; }
