b6891139711f529b89b9db0beb19b1247f3b0b14
Thanh <thanhnq_tts@rikkeisoft.com>
2022-09-19 12:03:54 +0700

    commit

---
+++ Quiz/Quiz.Web/Areas/Login/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quiz.Database.Repositories;
+using Quiz.Database.ViewModels;
+using Quiz.Web.Controllers;
+
+namespace Quiz.Web.Controllers
+{
+    [Area("Login")]
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IUnitOfWork _unitoWork;
+
+        public LoginController(IUnitOfWork unitoWork, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _unitoWork = unitoWork;
+        }
+
+        public IActionResult Index()
+        {
+            AccountsVM vM = new AccountsVM();
+            vM.accounts = _unitoWork.Account.GetAll().Where(p => p.IsDelete == "0");
+            return View(vM);
+        }
+    }
+}
--- Quiz/Quiz.Web/wwwroot/js/site.js
+++ Quiz/Quiz.Web/wwwroot/js/site.js
@@ -12,3 +12,27 @@ if (activePage != "/")
             }
         })
 }
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById('eye').addEventListener('click', function () {
+    if (pwShown == 0) {
+        console.log("0");
+        pwShown = 1;
+        show();
+    } else {
+        console.log("1");
+        pwShown = 0;
+        hide();
+    }
+}, false);
+
